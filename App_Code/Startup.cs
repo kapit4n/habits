b799586e8d8a19d75875092fae41169ccb37c36d
Luis Arce <luis.arce22@gmail.com>
2017-06-20 17:54:46 -0400

    Init

---
+++ App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Habits.Startup))]
+namespace Habits
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
