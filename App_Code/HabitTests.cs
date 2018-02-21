59b2cca2623d9dedfe9176da23b0b5eabeac3b66
Luis Arce <luis.arce22@gmail.com>
2018-02-21 17:55:33 -0400

    Writing the first unit test

---
+++ App_Code/HabitTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+
+public class HabitTests
+{
+  public HabitTests()
+  {
+  }
+
+  [Test]
+  void Test1()
+  {
+    Assert.AreEqual(1, 1, "This is the first example of tests");
+  }
+}
